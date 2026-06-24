dbc6a59a2ee0fcfbb41725cb7c140b0e5a7049a2
Jean-Baptiste-N <jeanbaptiste.nez@gmail.com>
2026-06-24 17:37:39 +0200

    fix persmissions static files

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,7 +5,8 @@ echo "==> Applying database migrations..."
 python manage.py migrate --noinput
 
 echo "==> Collecting static files..."
-python manage.py collectstatic --noinput
+rm -rf /app/staticfiles
+python manage.py collectstatic --noinput --clear
 
 echo "==> Starting server..."
 exec "$@"
