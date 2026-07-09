705266b04067735ee939871202f2827c755cc517
Jean-Baptiste-N <jeanbaptiste.nez@gmail.com>
2026-07-09 09:27:19 +0000

    test lancement dockers, pb sur web

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,7 +5,6 @@ echo "==> Applying database migrations..."
 python manage.py migrate --noinput
 
 echo "==> Collecting static files..."
-rm -rf /app/staticfiles
 python manage.py collectstatic --noinput --clear
 
 echo "==> Starting server..."
